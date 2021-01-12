512ee27f0df2e80f868af8059de8426fc9bae443
Kamil Przybylski <kprzybylski993@gmail.com>
2021-01-12 23:16:12 +0100

    fix(app) packages, initial provider

--- src/app/app-initial.provider.ts
+++ src/app/app-initial.provider.ts
@@ -1,20 +1,37 @@
-import { Store } from '@ngrx/store';
+import { select, Store } from '@ngrx/store';
 import { Injectable } from '@angular/core';
 import { AppState } from '@core/store';
 import { authLoginAction } from '@core/store/core/auth/auth.actions';
 import { layoutRouterLoginAction } from '@core/store/core/layout/layout.actions';
 import { AuthService } from '@core/services/auth.service';
+import { filter, take, tap } from 'rxjs/operators';
+import { getAuthIsLogging } from './core/store/core/auth/auth.selectors';
 
 @Injectable()
 export class AppInitialProvider {
   constructor(private store: Store<AppState>, private authService: AuthService) {}
 
-  load() {
+  load(): Promise<void> {
     const url = window.location.pathname;
     const token = this.authService.getToken();
 
     this.store.dispatch(layoutRouterLoginAction({ payload: { url } }));
     if (token) this.store.dispatch(authLoginAction({ payload: { url } }));
+
+    return new Promise((resolve, reject) => {
+      if (!token) resolve();
+      else {
+        this.store.dispatch(authLoginAction({ payload: { url } }));
+        this.store
+          .pipe(
+            select(getAuthIsLogging),
+            filter((isLogging) => !isLogging),
+            take(1),
+            tap(() => resolve())
+          )
+          .subscribe();
+      }
+    });
   }
 }
 
